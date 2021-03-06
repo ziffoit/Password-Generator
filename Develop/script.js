c72da02ad2d6a835a0aafb7c5cde8bfe6f73fff0
Kevin Hanson <ziffoit@gmail.com>
2021-03-05 19:31:03 -0700

    comments

--- Develop/script.js
+++ Develop/script.js
@@ -10,50 +10,60 @@ var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 function generatePassword() {
-
-  var userLength = parseInt(window.prompt('How many characters? [8-128]')); 
+  //making sure the user selects a number that is between 8 and 128 and alerting and kicking if not
+  var userLength = parseInt(window.prompt('How many characters? [8-128]'));
+  //console.log(userLength) 
   if ( isNaN(userLength) || userLength < 8 || userLength > 128) {
     //be angry and thrown an error
     window.alert("Length must be between 8 and 128")
     return
   }
-
+  
   var allowedChars = [];
-
-  var useLowercase = window.prompt('Do you want lowercase letters? (Y/N)').toUpperCase();
-   if (useLowercase === 'Y') {
+  //checking if you want lower case letters. converting response to uppercase
+  var useLowerCase = window.prompt('Do you want lowercase letters? (Y/N)').toUpperCase();
+  //console.log(useLowerCase)
+   if (useLowerCase === 'Y') {
      allowedChars = allowedChars.concat(lowerCase)
+     //console.log(allowedChars)
   }
-
-  var useUppercase = window.prompt('Do you want uppercase letters? (Y/N)').toUpperCase();
-   if (useUppercase === 'Y') {
-     allowedChars = allowedChars.concat(UpperCase)
+  //adding uppercase letters
+  var useUpperCase = window.prompt('Do you want uppercase letters? (Y/N)').toUpperCase();
+  //console.log(useUpperCase)
+   if (useUpperCase === 'Y') {
+     allowedChars = allowedChars.concat(upperCase)
+     //console.log(allowedChars)
   }
-
+  //adding numbers
   var useNumbers = window.prompt('Do you want numbers? (Y/N)').toUpperCase();
+  //console.log(useNumbers)
    if (useNumbers === 'Y') {
      allowedChars = allowedChars.concat(numbers)
+     //console.log(allowedChars)
   }
-
+  //adding special characters
   var useSpecialChar = window.prompt('Do you want special characters? (Y/N)').toUpperCase();
+  //console.log(useSpecialChar)
    if (useSpecialChar === 'Y') {
      allowedChars = allowedChars.concat(specialChar)
+     //console.log(allowedChars)
   }
 
 
    //  todo other character sets
-
-  if (allowedChars === []) {
-    window.alert("There is no secret cow level.")
+  //kicks you out if you don't choose any characters
+  if (allowedChars.length === 0) {
+    window.alert("You shall not pass(word).")
     return
-  } 
+  }
+  
 
   
 
   
 
   var password = "";
-
+  //selects a random character and adds it to password string
   for(var i=0; i < userLength; i++) {
     var index = Math.floor(Math.random() * allowedChars.length);
     var character = allowedChars[index];
@@ -65,11 +75,16 @@ function generatePassword() {
 }
 
 // Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  //if password is defined it runs
+  if (password) {
+    var passwordText = document.querySelector("#password");
+    //prints password to browser
+    passwordText.value = password;
+  }
+  
 
 }
 
